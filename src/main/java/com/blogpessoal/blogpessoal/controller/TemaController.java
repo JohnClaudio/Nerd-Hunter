27de7ce827d14e2847866adaf54945af432dbfd2
Johnclaudio <kidizinhu200@gmail.com>
2021-10-14 13:22:02 -0300

    adicionando novos endpoints em tema

--- src/main/java/com/blogpessoal/blogpessoal/controller/TemaController.java
+++ src/main/java/com/blogpessoal/blogpessoal/controller/TemaController.java
@@ -1,11 +1,12 @@
 package com.blogpessoal.blogpessoal.controller;
 
 import com.blogpessoal.blogpessoal.model.Tema;
+import com.blogpessoal.blogpessoal.model.Tema;
 import com.blogpessoal.blogpessoal.service.TemaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +20,32 @@ public class TemaController {
     @GetMapping
     public List<Tema> findAll(){
         return service.findAll();
+    }
+
+    @RequestMapping(value="/id/{id}", method= RequestMethod.GET)
+    public ResponseEntity<Tema>  getById(@PathVariable("id") Long id)
+    {
+        return ResponseEntity.ok().body(service.getById(id));
+
+    }
+
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity <List<Tema>> GetByTitulo(@PathVariable String titulo){
+        return ResponseEntity.ok(service.searchbyTitle(titulo));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Tema> insert (@RequestBody Tema Tema)
+    {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.insert(Tema));
+
+    }
+
+    @PutMapping()
+    public ResponseEntity<Tema> update (@RequestBody Tema Tema)
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(TemaService.insert(Tema));
+
     }
 }
