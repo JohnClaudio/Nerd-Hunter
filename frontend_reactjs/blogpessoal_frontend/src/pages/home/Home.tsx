fc297ad87ad30b702874a19b6b5ca8b4ed461d52
Johnclaudio <kidizinhu200@gmail.com>
2021-10-25 21:34:05 -0300

    REACT JS

---
+++ frontend_reactjs/blogpessoal_frontend/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'
+
+
+const Home = () =>{
+    return(
+        <div>
+        <h1>Home</h1>
+        <img  src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="imagem"/>
+        </div>
+    );
+}
+
+export default Home;
