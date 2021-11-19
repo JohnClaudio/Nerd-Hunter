82d654dc1ed36f4cb2ede126a3d58e9adf5f221d
João Claudio <61328786+JohnClaudio@users.noreply.github.com>
2021-11-19 04:28:48 -0300

    Update UsuarioController.java

--- src/main/java/com/blogpessoal/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/blogpessoal/blogpessoal/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
